431fc6e6995eebcf40d463b00442b86c2fa3fe29
lawalkeyd <lawalkeyd@gmail.com>
2021-02-15 14:26:19 +0100

    Spider edit

--- ali/spiders/ali_spiders.py
+++ ali/spiders/ali_spiders.py
@@ -11,6 +11,14 @@ class AliSpider(scrapy.Spider):
         super(AliSpider, self).__init__(*args, **kwargs)
 
     def parse(self, response):
+        title = response.css('.module-pdp-title ::text').extract()
+        images = response.css('.main-image-thumb-ul img::attr(src)').extract()
+        price = response.css('.ma-ref-price span::text').extract()
+        types = response.css('.sku-attr-val span::text').extract()        
         i = {}
         i['url'] = response.url
+        i['title'] = title
+        i['images'] = images
+        i['price'] = price
+        i['types'] = types
         return i
